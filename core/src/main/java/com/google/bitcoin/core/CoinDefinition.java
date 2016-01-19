a47e6c53f31adc52de83501071254bccf87791d1
HashEngineering <hashengineeringsolutions@gmail.com>
2016-01-18 17:21:10 -0800

    Updated coin definition

--- core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
+++ core/src/main/java/com/google/bitcoin/core/CoinDefinition.java
@@ -61,7 +61,7 @@ public class CoinDefinition {
     public static final BigInteger DEFAULT_MIN_TX_FEE = BigInteger.valueOf(10000);   // MIN_TX_FEE
     public static final BigInteger DUST_LIMIT = BigInteger.valueOf(10000); //main.h CTransaction::GetMinFee        0.01 coins
 
-    public static final int PROTOCOL_VERSION = 70001;          //version.h PROTOCOL_VERSION
+    public static final int PROTOCOL_VERSION = 70002;          //version.h PROTOCOL_VERSION
     public static final int MIN_PROTOCOL_VERSION = 209;        //version.h MIN_PROTO_VERSION
     public static final int INIT_PROTO_VERSION = 209;            //version.h
 
