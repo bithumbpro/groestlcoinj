c6ff57cbb1c52cc5324c71f425bfcbeb8576962e
HashEngineering <hashengineeringsolutions@gmail.com>
2016-05-01 15:27:57 -0700

    added xpub, xprv back in MainNetParams

--- core/src/main/java/org/bitcoinj/params/MainNetParams.java
+++ core/src/main/java/org/bitcoinj/params/MainNetParams.java
@@ -39,8 +39,11 @@ public class MainNetParams extends AbstractBitcoinNetParams {
         p2shHeader = CoinDefinition.p2shHeader;
         acceptableAddressCodes = new int[] { addressHeader, p2shHeader};
 
+
         port = CoinDefinition.Port;
         packetMagic = CoinDefinition.PacketMagic;
+        bip32HeaderPub = 0x0488B21E; //The 4 byte header that serializes in base58 to "xpub".
+        bip32HeaderPriv = 0x0488ADE4; //The 4 byte header that serializes in base58 to "xprv"
         genesisBlock.setDifficultyTarget(CoinDefinition.genesisBlockDifficultyTarget);
         genesisBlock.setTime(CoinDefinition.genesisBlockTime);
         genesisBlock.setNonce(CoinDefinition.genesisBlockNonce);
