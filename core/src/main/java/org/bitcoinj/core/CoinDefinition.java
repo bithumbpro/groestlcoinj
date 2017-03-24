08b9975d1cd44aeccc7a71478d16ebbd36443e01
HashEngineering <hashengineeringsolutions@gmail.com>
2017-03-24 07:11:13 -0700

    Update protocol version

--- core/src/main/java/org/bitcoinj/core/CoinDefinition.java
+++ core/src/main/java/org/bitcoinj/core/CoinDefinition.java
@@ -76,7 +76,7 @@ public class CoinDefinition {
     public static final Coin DEFAULT_MIN_TX_FEE = Coin.valueOf(10000);   // MIN_TX_FEE
     public static final Coin DUST_LIMIT = Coin.valueOf(10000); //main.h CTransaction::GetMinFee        0.01 coins
 
-    public static final int PROTOCOL_VERSION = 70002;          //version.h PROTOCOL_VERSION
+    public static final int PROTOCOL_VERSION = 70015;          //version.h PROTOCOL_VERSION
     public static final int MIN_PROTOCOL_VERSION = 209;        //version.h MIN_PROTO_VERSION
     public static final int INIT_PROTO_VERSION = 209;            //version.h
 
