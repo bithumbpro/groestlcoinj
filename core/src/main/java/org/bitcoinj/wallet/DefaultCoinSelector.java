816b1a1cc27a08143f5384a0c3066ed1052ca614
anton <admin@btcontract.com>
2018-07-20 11:18:36 +0300

    Back to default coin selector

--- core/src/main/java/org/bitcoinj/wallet/DefaultCoinSelector.java
+++ core/src/main/java/org/bitcoinj/wallet/DefaultCoinSelector.java
@@ -63,9 +63,16 @@ public class DefaultCoinSelector implements CoinSelector {
         Collections.sort(outputs, new Comparator<TransactionOutput>() {
             @Override
             public int compare(TransactionOutput a, TransactionOutput b) {
+                int depth1 = a.getParentTransactionDepthInBlocks();
+                int depth2 = b.getParentTransactionDepthInBlocks();
                 Coin aValue = a.getValue();
                 Coin bValue = b.getValue();
-                int c2 = aValue.compareTo(bValue);
+                BigInteger aCoinDepth = BigInteger.valueOf(aValue.value).multiply(BigInteger.valueOf(depth1));
+                BigInteger bCoinDepth = BigInteger.valueOf(bValue.value).multiply(BigInteger.valueOf(depth2));
+                int c1 = bCoinDepth.compareTo(aCoinDepth);
+                if (c1 != 0) return c1;
+                // The "coin*days" destroyed are equal, sort by value alone to get the lowest transaction size.
+                int c2 = bValue.compareTo(aValue);
                 if (c2 != 0) return c2;
                 // They are entirely equivalent (possibly pending) so sort by hash to ensure a total ordering.
                 BigInteger aHash = a.getParentTransactionHash().toBigInteger();
